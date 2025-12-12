638b18508bf1cc3ea5d53f8e761769e3c9016a69
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2025-12-11 23:19:51 -0300

    refactor: removido mock e adicionada toolbar ao histórico

--- app/src/main/java/com/example/imfitplus/HistoricoActivity.kt
+++ app/src/main/java/com/example/imfitplus/HistoricoActivity.kt
@@ -1,9 +1,11 @@
 package com.example.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.imfitplus.adapter.PessoaAdapter
 import com.example.imfitplus.controller.MainController
+import com.example.imfitplus.databinding.ActivityGastoCaloricoBinding
 import com.example.imfitplus.databinding.ActivityHistoricoBinding
 import com.example.imfitplus.entities.Pessoa
 import com.example.imfitplus.enums.NivelAtividade
@@ -25,21 +27,18 @@ class HistoricoActivity : AppCompatActivity() {
         binding = ActivityHistoricoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        inicializaBotaoMenu(binding)
+
         lista = pessoaController.getAllDados()
 
         val adapter = PessoaAdapter(this, lista)
         binding.myListView.adapter = adapter
     }
 
-    private fun carregarHistoricoFake(): MutableList<Pessoa> {
-        return mutableListOf(
-            Pessoa("Ronaldo", 40, Sexo.MASCULINO,
-                NivelAtividade.INTENSO,
-                1.85, 120.0, 30.0, 20.4, 2132.3, 22.0),
-
-            Pessoa("Zara Vitória", 30, Sexo.FEMININO,
-                NivelAtividade.MODERADO,
-                1.60, 65.0, 0.0, 0.0, 0.0, 0.0)
-        )
+    private fun inicializaBotaoMenu(activity: ActivityHistoricoBinding) {
+        activity.menuBtn.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
