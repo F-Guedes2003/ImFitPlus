a5a14931a49d95c0c960ac6f927c207a26d584e8
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2025-12-11 23:14:14 -0300

    refator: resumo saude

--- app/src/main/java/com/example/imfitplus/ResumoSaudeActivity.kt
+++ app/src/main/java/com/example/imfitplus/ResumoSaudeActivity.kt
@@ -1,11 +1,13 @@
 package com.example.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.imfitplus.databinding.ActivityFormBinding
+import com.example.imfitplus.databinding.ActivityPesoIdealBinding
 import com.example.imfitplus.databinding.ActivityResumoSaudeBinding
 import com.example.imfitplus.entities.Pessoa
 
@@ -17,9 +19,18 @@ class ResumoSaudeActivity : AppCompatActivity() {
         arsb = ActivityResumoSaudeBinding.inflate(layoutInflater)
         setContentView(arsb.root)
 
+        inicializaBotaoMenu(arsb)
+
         val pessoa = intent.getParcelableExtra<Pessoa>("Pessoa")
         arsb.valorImc.text = "%.2f".format(pessoa!!.imc)
         arsb.valorPesoIdeal.text = "%.2f".format(pessoa.pesoIdeal)
         arsb.tmbValor.text = "%.2f".format(pessoa.taxaMetabolica)
+    }
+
+    private fun inicializaBotaoMenu(activity: ActivityResumoSaudeBinding) {
+        activity.menuBtn.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
